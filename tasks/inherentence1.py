0a5cd4e9f9713d13edb62f8c18c4da14cd17441f
guspatrick <75298442+guspatrick@users.noreply.github.com>
2021-01-04 00:34:17 +0100

    Update inherentence1.py
    Added classes, inheritance, constructors, overridden the __new__ method, and implemented assertions to test encapsulation.

--- tasks/inherentence1.py
+++ tasks/inherentence1.py
@@ -41,3 +41,116 @@ be able to create a species of fish, e.g. salmon without specifying water_type()
 
 """
 
+
+# Define classes.
+class Animal:
+    
+    def __init__(self, cat, live, predator):
+        self.cat = cat
+        self.live = live
+        self.predator = predator
+    
+    def __new__(cls, *args, **kwargs):
+        if cls is __class__:
+            raise TypeError(f'Base class {__class__} cannot be instantiated.')
+        return object.__new__(cls, *args, **kwargs)
+
+        
+class Fish(Animal):
+    
+    __reproduce = 'laying eggs'
+    __water_type = 'freshwater'
+
+    def __init__(self, predator):
+        super().__init__('fish', 'sea', predator)
+    
+    def __new__(cls, *args, **kwargs):
+        if cls is __class__:
+            raise TypeError(f'Base class {__class__} cannot be instantiated.')
+        return object.__new__(cls, *args, **kwargs)
+        
+    def reproduce(self):
+        return __class__.__reproduce
+
+    def water_type(self):
+        return __class__.__water_type
+
+    
+class Tuna(Fish):
+    
+    def __init__(self):
+        super().__init__(True)
+
+        
+class Mammal(Animal):
+    
+    __reproduce = 'giving birth'
+    __talk = 'squeak squeak!'
+    
+    def __init__(self, live, predator):
+        super().__init__('mammal', live, predator)
+
+    def __new__(cls, *args, **kwargs):
+        if cls is __class__:
+            raise TypeError(f'Base class {__class__} cannot be instantiated.')
+        return object.__new__(cls, *args, **kwargs)
+    
+    def reproduce(self):
+        return __class__.__reproduce
+    
+    def talk(self):
+        return __class__.__talk
+
+    
+class Dolphin(Mammal):
+    
+    def __init__(self):
+        super().__init__('sea', True)
+
+
+# Instantiate object of Tuna.
+billy = Tuna()
+
+
+if __name__ == '__main__:
+
+    # Print values.
+    print(billy.live)
+    print(billy.predator)
+    print(billy.cat)
+    print(billy.water_type())
+    print(billy.reproduce())
+
+    # Perform assertions.
+    assert billy.live == 'sea'
+    assert billy.predator == True
+    assert billy.cat == 'fish'
+    assert billy.water_type() == 'freshwater'
+    assert billy.reproduce() == 'laying eggs'
+
+
+    # Instantiate object of Dolphin.
+    steve = Dolphin()
+
+    # Print values.
+    print(steve.live)
+    print(steve.predator)
+    print(steve.cat)
+    print(steve.reproduce())
+    print(steve.talk())
+
+    # Perform assertions.
+    assert steve.live == 'sea'
+    assert steve.predator == True
+    assert steve.cat == 'mammal'
+    assert steve.reproduce() == 'giving birth'
+    assert steve.talk() == 'squeak squeak!'
+
+
+    # Expect TypeErrors.
+    bob = Fish()
+    a = Animal()
+
+    # Expect AttributeError.
+    steve.water_type()
+    billy.talk()
